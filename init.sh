7af4b019a3ff5a85da33c4c4352f5f2348b2e12f
sedy74 <karpelyanskiy@gmail.com>
2018-08-14 18:30:49 +0000

    2018_08_14_2

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@
 
 git config --global user.email "karpelyanskiy@gmail.com"
 git config --global user.name "sedy74"
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 /home/box/web/hello:application
